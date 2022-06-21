91fe2c2ffda3223fca3f1b14a00b705cf8c3a7e0
SumitTiket <106805415+SumitTiket@users.noreply.github.com>
2022-06-22 01:11:26 +0530

    Add files via upload

---
+++ sw/src/test/java/com/sw/sw/SwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw.sw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
