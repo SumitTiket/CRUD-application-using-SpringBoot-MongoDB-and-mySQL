9326c2473283a493a85eed7ca995d3137cbce465
SumitTiket <106805415+SumitTiket@users.noreply.github.com>
2022-06-22 15:54:35 +0530

    Post bug resolved

--- sw/src/main/java/com/sw/sw/controller/StudentController.java
+++ sw/src/main/java/com/sw/sw/controller/StudentController.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
+@RestController
 public class StudentController {
 
     @Autowired
